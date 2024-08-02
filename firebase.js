2d2f77166eeebdf12030b8a3f221158f50a39ccf
Luis Gutierrez <113130547+Gigituier@users.noreply.github.com>
2024-08-02 08:46:39 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'; // Corrected import
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBV4p0TTHSOHqjS3nzp9cXzn62GI9sCO2c",
-  authDomain: "iventory-app-d0f62.firebaseapp.com",
-  projectId: "iventory-app-d0f62",
-  storageBucket: "iventory-app-d0f62.appspot.com",
+  apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  projectId: "<projectId>",
+  storageBucket: "<storageBucket>",
   messagingSenderId: "364667044550",
-  appId: "1:364667044550:web:c76875d3200aeb818b4dc7"
+  appId: "<app-id>"
 };
 
 // Initialize Firebase
